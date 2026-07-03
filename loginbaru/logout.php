7ddb17740646dcab58a8fc18c60a370c953d82ab
fazriel <fazriel.keysha@gmail.com>
2026-07-03 13:55:06 +0700

    modify some file

--- loginbaru/logout.php
+++ loginbaru/logout.php
@@ -3,11 +3,11 @@
 // remove all session variables
 session_start();
 
-session_unset(); 
+session_unset();
 
-// destroy the session 
-session_destroy(); 
+// destroy the session
+session_destroy();
 
 header("location:login.php");
- 
+
 ?>
--- newajax/ceklogin.php
+++ newajax/ceklogin.php
@@ -1,35 +1,29 @@
 <?php
-	session_start();
-	include "config/koneksi.php";
-	 
-	
-	$username=$_POST['userlog']; 
-	$passs=$_POST['passlog']; 
-	 
-	
-	if (($username == ""  ) or ($passs==""))
-	{ 
-		echo "<script>alert('Username dan Password tidak boleh kosong');document.location='login.php';</script>";
-	}
-	else
-	{
-		$result=mysqli_query($conn,"SELECT * FROM user WHERE (username ='".$username."') AND (password ='".$passs."')");
+session_start();
+include "config/koneksi.php";
 
-		if(mysqli_num_rows($result) > 0)
-		{ 
-		  $row=mysqli_fetch_assoc($result);		
-		  $username =$row['username'];
-		  $passs    =$row['password'];
-		 
-		  $_SESSION["user"] = $username;
-		  $_SESSION["pass"] = $passs;
-		  
-		  header("location:dashboard.php"); 
-		  
-		}
-		else
-		{
-			echo "<script>alert('Login tidak berhasil, username dan password tidak valid'); document.location='login.php';</script>";
-		}
+
+$username = $_POST['userlog'];
+$passs = $_POST['passlog'];
+
+
+if (($username == "") or ($passs == "")) {
+	echo "<script>alert('Username dan Password tidak boleh kosong');document.location='login.php';</script>";
+} else {
+	$result = mysqli_query($conn, "SELECT * FROM user WHERE (username ='" . $username . "') AND (password ='" . $passs . "')");
+
+	if (mysqli_num_rows($result) > 0) {
+		$row = mysqli_fetch_assoc($result);
+		$username = $row['username'];
+		$passs = $row['password'];
+
+		$_SESSION["user"] = $username;
+		$_SESSION["pass"] = $passs;
+
+		header("location:dashboard.php");
+
+	} else {
+		echo "<script>alert('Login tidak berhasil, username dan password tidak valid'); document.location='login.php';</script>";
 	}
+}
 ?>
